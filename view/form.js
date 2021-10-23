f0330c25b2794d692f24cadb44b89a7164e75f3d
Gabi <gabipaez572018@gmail.com>
2021-10-23 12:01:27 -0500

    pequeño cambio para traer otros

--- view/form.js
+++ view/form.js
@@ -4,18 +4,18 @@ export const form = () => {
     divForm.setAttribute('class', 'div-form ');
     const viewForm = ` 
     <section class=" section-hdr center-align">
-      <img src='img/logo.png' alt='logo' id='logo' class='responsive-img' width='120'/>
+      <img src='img/logo.png' alt='logo' id='logo' class='responsive-img' width='100'/>
       <p class= 'title'>Caribe</p>
       <p class= 'sub-title'>Calcula tu huella de carbono</p>
     </section>
     <div class= ' div-selects responsive center-align row  '>
-    <select class="browser-default col s6 responsive ">
+    <select class="browser-default col s5 responsive ">
       <option value="" disabled selected>Choose your option</option>
       <option value="1">Option 1</option>
       <option value="2">Option 2</option>
       <option value="3">Option 3</option>
     </select>
-    <select class="browser-default col s6 responsive">
+    <select class="browser-default col s5 responsive">
       <option value="" disabled selected>Choose your option</option>
       <option value="1">Option 1</option>
       <option value="2">Option 2</option>
