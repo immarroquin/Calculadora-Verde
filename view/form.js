8a65377a973000f42f36627a9e9e2131ff65b048
immarroquin <lorenamarroquin56@gmail.com>
2021-10-23 07:44:05 -0500

    empezamos estilos con materialize

--- view/form.js
+++ view/form.js
@@ -7,15 +7,15 @@ export const form = () => {
       <img src='img/logo.png' alt='logo' class='img-logo'/>
       <p>Calcula tu huella de carbono</p>
     </section>
-    <div class='container-select'>
-      <select class='select'>
-        <option value=''>Selecciona tu sector</option>
-        <option value='bbb'>bbb</option>
-        <option value='aaa'>aaa</option>
-        <option value='bbb'>bbb</option>
-        <option value='aaa'>aaa</option>
-      </select>
-    </div>
+    <div class="input-field col s12">
+    <select>
+      <option value="" disabled selected>Choose your option</option>
+      <option value="1">Option 1</option>
+      <option value="2">Option 2</option>
+      <option value="3">Option 3</option>
+    </select>
+    <label>Materialize Select</label>
+  </div>
     <section class="form-section">
       <form action="">
         <p>Pregunta 1</p>
@@ -29,7 +29,7 @@ export const form = () => {
         <hr class="linea" />
         <p>Pregunta 4</p>
         <input type="text" placeholder="12345kg" />
-        <a href="#/solutions">Calcular</a>
+        <button href="#/solutions">Calcular</button>
       </form>
     </section> `
     divForm.innerHTML = viewForm;
