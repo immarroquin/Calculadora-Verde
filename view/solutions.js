db3f87b458e863235790fff6e8534dd658803e5b
Gabi <gabipaez572018@gmail.com>
2021-10-23 18:32:40 -0500

    secciones de soluciones con estructura simple

--- view/solutions.js
+++ view/solutions.js
@@ -6,7 +6,24 @@ export const solutions = () => {
       <img src='img/logo.png' alt='logo' id='logo' class='responsive-img' width='120'/>
       <p class= 'title'>Caribe</p>
       <p class= 'sub-title'>Tu huella de carbono es</p>
-      <a href='' class="btn-clo waves-effect waves-light btn">NUEVO CALCULO</a>
+    </section>
+    <div class=' content-result'>
+  <h2 class= 'result'>23</h2>
+  <p class='unit-mass'>gs</p>
+</div>
+<section class=" center-align">
+<div class='content-equivalence'>
+<h5>EQUIVALENCIA</h5>
+ </div>
+ <div class='content-solutions'>
+<h5>Soluciones</h5>
+ </div>
+ <div class='content-saving'>
+<h5>AHORRO</h5>
+ </div>
+</section>
+    <section class=" section-hdr center-align">
+    <a href='' class="btn-clo waves-effect waves-light btn">NUEVO CALCULO</a>
     </section>`
     divSolutions.innerHTML = viewSolutions;
     return divSolutions
